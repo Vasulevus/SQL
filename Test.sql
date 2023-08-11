0abdb3fabc5668b312d106319db355820bfa304e
Vasulevus <Vasulevus@gmail.com>
2023-08-11 16:14:43 +0300

    Added 2.1 in progress

--- Test.sql
+++ Test.sql
@@ -1,3 +1,36 @@
+--структура page_type та event_type
+SELECT 
+      DISTINCT([page_type])
+      ,[event_type]
+  FROM [test].[dbo].[data_set_da_test]
+
+
+
+-- загальний запит до лійки
+SELECT 
+	[pages],
+	[event_type],
+	COUNT(*) AS [opened_pages]
+FROM
+	(SELECT
+		CASE 
+		WHEN page_type = 'search_listing_page' THEN 'listing_page'
+		ELSE page_type END as pages,
+		[event_type]
+	FROM
+		test.dbo.data_set_da_test) AS A
+GROUP BY [pages],[event_type]
+ORDER BY [opened_pages] DESC
+
+SELECT 
+	[page_type],
+	[event_type],
+	COUNT(*) AS [opened_pages]
+FROM
+	test.dbo.data_set_da_test AS A
+GROUP BY [page_type],[event_type]
+ORDER BY [opened_pages] DESC
+
 -- запит для з'ясування поточного стану різних сценаріїв
 WITH listing AS
 (
@@ -49,3 +82,19 @@ SELECT
 from types_join
 GROUP BY lp, pp, op
 ORDER BY numbers
+
+
+2
+
+2.1 
+--допрацювати
+SELECT
+	[user],
+	[session],
+	[page_type],
+	COUNT(*) as c
+FROM 
+	test.dbo.data_set_da_test
+WHERE
+	[session] NOT IN ( анти джойн із зі списком сесій, в яких не було event_type add_to cart чи page_type = order_page)
+GROUP BY [user], [page_type],[session];
